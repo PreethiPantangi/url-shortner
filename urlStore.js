cc8641095fd50d0718bbfeeccd2eae2e011eacb6
preethipantangi <pantangisaipreethi@gmail.com>
2024-09-28 13:33:40 -0400

    Modified url store

--- urlStore.js
+++ urlStore.js
@@ -1,26 +1,47 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
-const urlStorePath = path.join(__dirname, 'urlStore.json');
+// const urlStorePath = path.join(__dirname, 'urlStore.json');
 
-let urlStore = {};
+// let urlStore = {};
 
-async function loadUrlStore() {
-    if (fs.existsSync(urlStorePath)) {
-        const data = fs.readFileSync(urlStorePath, 'utf-8');
-        urlStore = JSON.parse(data);
-    }
-}
+// async function loadUrlStore() {
+//     if (fs.existsSync(urlStorePath)) {
+//         const data = fs.readFileSync(urlStorePath, 'utf-8');
+//         urlStore = JSON.parse(data);
+//     }
+// }
 
-async function saveUrlStore() {
-    fs.writeFileSync(urlStorePath, JSON.stringify(urlStore, null, 2), 'utf-8');
-}
+// async function saveUrlStore() {
+//     fs.writeFileSync(urlStorePath, JSON.stringify(urlStore, null, 2), 'utf-8');
+// }
 
-loadUrlStore();
+// loadUrlStore();
+
+// function saveUrl(urlCode, originalUrl) {
+//     urlStore[urlCode] = originalUrl;
+//     saveUrlStore(); 
+// }
+
+// function getUrl(urlCode) {
+//     return urlStore[urlCode];
+// }
+
+// function getOriginalUrl(originalUrl) {
+//     return urlStore[originalUrl];
+// }
+
+// module.exports = {
+//     saveUrl,
+//     getUrl,
+//     getOriginalUrl,
+// };
+
+// urlStore.js
+const urlStore = {};
 
 function saveUrl(urlCode, originalUrl) {
     urlStore[urlCode] = originalUrl;
-    saveUrlStore(); 
 }
 
 function getUrl(urlCode) {

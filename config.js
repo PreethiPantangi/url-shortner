37a05dc363397b873f1afe56c5c79c5635645e36
preethipantangi <pantangisaipreethi@gmail.com>
2024-09-28 13:22:11 -0400

    Modified shorten url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     "port": process.env.PORT || 5000,
-    "host": process.env.HOST || "localhost"
+    "host": process.env.HOST || "url-shortner-tau-five.vercel.app"
 }
 
 module.exports = config;
